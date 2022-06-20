a491d37c124037162f66b840c87c862f07c8ac9f
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2022-06-20 11:06:02 -0300

    CRUD básico

--- src/main/java/apenado/ControladorApenado.java
+++ src/main/java/apenado/ControladorApenado.java
@@ -8,27 +8,46 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControladorApenado {
 
     @Autowired
-    private ServicoApenado service;
+    private RepositorioApenado service;
 
-    @GetMapping("/apenado")
-    public String greetingForm(Model model) {
+    @GetMapping("/listarApenados")
+    public String listarApenados(Model model) {
+        model.addAttribute("lista", service.findAll());
+        return "result";
+    }
+
+    @GetMapping("/inserirApenadoForm")
+    public String inserirApenadoForm(Model model) {
         model.addAttribute("apenado", new Apenado());
         return "apenado";
     }
 
-    @PostMapping("/apenado")
-    public String inserirApenado(@Valid Apenado apenado, BindingResult bindingResult, Model model) {
+    @PostMapping("/armazenarApenado")
+    public String armazenarApenado(@Valid Apenado apenado, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "apenado";
         }
-        service.inserirApenado(apenado);
-        model.addAttribute("apenado", apenado);
-        model.addAttribute("lista", service.listarApenados());
-        return "result";
+        service.save(apenado);
+        return "redirect:/listarApenados";
     }
+
+    @GetMapping("/alterarApenadoForm")
+    public String alterarApenadoForm(@RequestParam String cpf, Model model) {
+        Apenado apenado = service.findById(cpf).get();
+		model.addAttribute("apenado", apenado);
+        return "apenado";
+    }
+
+    @GetMapping("/removerApenado")
+    public String removerApenado(@RequestParam String cpf, Model model) {
+        service.deleteById(cpf);
+        return "redirect:/listarApenados";
+    }
+
 }
--- src/main/java/apenado/RepositorioApenado.java
+++ src/main/java/apenado/RepositorioApenado.java
@@ -1,14 +1,9 @@
 package apenado;
 
-import java.util.List;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "apenados", path = "apenados")
-public interface RepositorioApenado extends PagingAndSortingRepository<Apenado, String> {
+public interface RepositorioApenado extends JpaRepository<Apenado, String> {
 
-    List<Apenado> findByNome(@Param("nome") String nome);
-    List<Apenado> findAll();
 }
