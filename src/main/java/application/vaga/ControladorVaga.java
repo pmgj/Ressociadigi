cee71a133520e9c6bb9c72d66a54ad75b45af5af
José Alfredo <netoalfredo9@gmail.com>
2022-11-10 00:32:40 -0300

    Melhoria no tratamento de exceção de duplicidade de apenado atrelado a vaga preenchida

--- src/main/java/application/vaga/ControladorVaga.java
+++ src/main/java/application/vaga/ControladorVaga.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -57,8 +58,11 @@ public class ControladorVaga {
 		try {
 			repVagaPreenchida.save(vagaPreenchida);
 		} catch(Exception e) {
+			model.addAttribute("listaApenados", repApenado.findAll());
+			model.addAttribute("listaVagas", service.findAll());
+			bindingResult.rejectValue("apenado", null, null, "Você tentou atribuir uma vaga a um apenado que já está empregado.");
 			System.err.print(e.getMessage());
-			return "redirect:/preencherVaga";
+			return "alocarVagaApenado";
 		}		
 		return "redirect:/listarVagasPreenchidas";
 	}
