c2cc6913aa5d9638d4757fd5c80c0bd08deda346
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2022-11-13 18:54:24 -0300

    atualização

---
+++ src/main/resources/static/js/empresa.js
@@ -0,0 +1,18 @@
+
+//mudar leganda
+
+let legenda = document.querySelector('.legenda')
+let select = document.querySelector('#select-tipoBusca')
+
+function teste (){
+     if (select.options[0].selected){
+        legenda.innerHTML = 'dhiw3hed'
+    }
+     if (select.options[3].selected){
+        legenda.innerHTML = '2'
+    }
+
+} 
+teste() 
+
+
