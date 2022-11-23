1adbc90631cfcce3d16dd1962a5bdcaf6b283520
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2022-11-23 09:06:01 -0300

    Correct imports

--- src/main/java/application/vaga/ControladorVaga.java
+++ src/main/java/application/vaga/ControladorVaga.java
@@ -9,7 +9,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,38 +19,38 @@ import application.empresa.RepositorioEmpresa;
 
 @Controller
 public class ControladorVaga {
-	
+
 	@Autowired
 	private RepositorioVaga service;
-	
+
 	@Autowired
 	private RepositorioEmpresa repEmpresa;
-	
+
 	@Autowired
 	private RepositorioVagaPreenchida repVagaPreenchida;
-	
+
 	@Autowired
 	private RepositorioApenado repApenado;
-	
-	
-	//Seção de Vagas Preenchidas
+
+	// Seção de Vagas Preenchidas
 	@GetMapping("/listarVagasPreenchidas")
 	public String listarVagasPreenchidas(Model model, @PageableDefault(page = 0, size = 2) Pageable pageable) {
 		Page<VagaPreenchida> pgVagaPreenchida = repVagaPreenchida.findAll(pageable);
-		
-		//Manipulação das Páginas
+
+		// Manipulação das Páginas
 		int numPaginaAtual = pageable.getPageNumber() + 1;
 		int numTotalPaginas = pgVagaPreenchida.getTotalPages();
-        model.addAttribute("pageCounter", "Página " + numPaginaAtual + " de " + numTotalPaginas);
-        model.addAttribute("nextPage", ((pageable.getPageNumber() + 1) > numTotalPaginas - 1) 
-				? pageable.getPageNumber() : pageable.getPageNumber() + 1 );
-        model.addAttribute("previousPage", pageable.getPageNumber() - 1);
-        model.addAttribute("quantidadePaginas", numTotalPaginas);
-		
-		model.addAttribute("listaVagasPreenchidas", pgVagaPreenchida);		
+		model.addAttribute("pageCounter", "Página " + numPaginaAtual + " de " + numTotalPaginas);
+		model.addAttribute("nextPage", ((pageable.getPageNumber() + 1) > numTotalPaginas - 1)
+				? pageable.getPageNumber()
+				: pageable.getPageNumber() + 1);
+		model.addAttribute("previousPage", pageable.getPageNumber() - 1);
+		model.addAttribute("quantidadePaginas", numTotalPaginas);
+
+		model.addAttribute("listaVagasPreenchidas", pgVagaPreenchida);
 		return "listarVagasPreenchidas";
 	}
-	
+
 	@GetMapping("/preencherVaga")
 	public ModelAndView preencherVaga(Model model) {
 		VagaPreenchida vagaPreenchida = new VagaPreenchida();
@@ -62,87 +61,92 @@ public class ControladorVaga {
 		mav.addObject("vagaPreenchida", vagaPreenchida);
 		return mav;
 	}
-	
+
 	@PostMapping("/armazenarVagaPreenchida")
-	public String armazenarVagaPreenchida(@Valid VagaPreenchida vagaPreenchida, BindingResult bindingResult, Model model) {
-		if(bindingResult.hasErrors()) {
+	public String armazenarVagaPreenchida(@Valid VagaPreenchida vagaPreenchida, BindingResult bindingResult,
+			Model model) {
+		if (bindingResult.hasErrors()) {
 			return "alocarVagaApenado";
 		}
 		try {
 			repVagaPreenchida.save(vagaPreenchida);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			model.addAttribute("listaApenados", repApenado.findAll());
 			model.addAttribute("listaVagas", service.findAll());
-			bindingResult.rejectValue("apenado", null, null, "Você tentou atribuir uma vaga a um apenado que já está empregado.");
+			bindingResult.rejectValue("apenado", null, null,
+					"Você tentou atribuir uma vaga a um apenado que já está empregado.");
 			System.err.print(e.getMessage());
 			return "alocarVagaApenado";
-		}		
+		}
 		return "redirect:/listarVagasPreenchidas";
 	}
-	
+
 	@GetMapping("/alterarVagaPreenchida")
 	public String alterarVagaPreenchida(@RequestParam int id, Model model) {
 		model.addAttribute("vagaPreenchida", repVagaPreenchida.findById(id).get());
-		model.addAttribute("listaVagas", service.findById(repVagaPreenchida.findById(id).get().getVaga().getId()).get());//listaVagas
-		model.addAttribute("listaApenados", repApenado.findById(repVagaPreenchida.findById(id).get().getApenado().getCpf()).get());//listaApenados
+		model.addAttribute("listaVagas",
+				service.findById(repVagaPreenchida.findById(id).get().getVaga().getId()).get());// listaVagas
+		model.addAttribute("listaApenados",
+				repApenado.findById(repVagaPreenchida.findById(id).get().getApenado().getCpf()).get());// listaApenados
 		return "alocarVagaApenado";
 	}
-	
+
 	@GetMapping("/removerVagaPreenchida")
 	public String removerVagaPreenchida(@RequestParam int id, Model model) {
 		VagaPreenchida vagaPreenchida = repVagaPreenchida.findById(id).get();
 		repVagaPreenchida.delete(vagaPreenchida);
 		return "redirect:/listarVagasPreenchidas";
 	}
-	
-	//Seção de Vagas
-    @GetMapping("/inserirVaga")
-    public ModelAndView inserirVaga(Model model) {
-    	Vaga vaga = new Vaga();
-    	ModelAndView mav = new ModelAndView("vagas");
-    	mav.addObject("vaga", vaga);
-    	model.addAttribute("listaEmpresas", repEmpresa.findAll());
-    	return mav;
-    }
-    
-    @PostMapping("/armazenarVaga")
-    public String armazenarVaga(@Valid Vaga vaga, BindingResult bindingResult, Model model){
-    	if (bindingResult.hasErrors()) {
-            return "vagas";
-        }
-        service.save(vaga);
-        return "redirect:/listarVagas";
-    }
-    
-    @GetMapping("/listarVagas")
-    public String listarVagas(Model model, @PageableDefault(page = 0, size = 2) Pageable pageable) {
-    	Page<Vaga> pgVagas = service.findAll(pageable);
-    	
-    	
-    	//Manipulação de Páginas
-    	int numPaginaAtual = pageable.getPageNumber() + 1;
+
+	// Seção de Vagas
+	@GetMapping("/inserirVaga")
+	public ModelAndView inserirVaga(Model model) {
+		Vaga vaga = new Vaga();
+		ModelAndView mav = new ModelAndView("vagas");
+		mav.addObject("vaga", vaga);
+		model.addAttribute("listaEmpresas", repEmpresa.findAll());
+		return mav;
+	}
+
+	@PostMapping("/armazenarVaga")
+	public String armazenarVaga(@Valid Vaga vaga, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "vagas";
+		}
+		service.save(vaga);
+		return "redirect:/listarVagas";
+	}
+
+	@GetMapping("/listarVagas")
+	public String listarVagas(Model model, @PageableDefault(page = 0, size = 2) Pageable pageable) {
+		Page<Vaga> pgVagas = service.findAll(pageable);
+
+		// Manipulação de Páginas
+		int numPaginaAtual = pageable.getPageNumber() + 1;
 		int numTotalPaginas = pgVagas.getTotalPages();
-        model.addAttribute("pageCounter", "Página " + numPaginaAtual + " de " + numTotalPaginas);
-        model.addAttribute("nextPage", ((pageable.getPageNumber() + 1) > numTotalPaginas - 1) 
-				? pageable.getPageNumber() : pageable.getPageNumber() + 1 );
-        model.addAttribute("previousPage", pageable.getPageNumber() - 1);
-        model.addAttribute("quantidadePaginas", numTotalPaginas);
-    	
-    	model.addAttribute("listaVagas", pgVagas);
-    	return "listarVagas";
-    }
-    
-    @GetMapping("/alterarVaga")
-    public String alterarVaga(@RequestParam int id, Model model) {
-    	model.addAttribute("vaga", service.findById(id));
-    	model.addAttribute("listaEmpresas", repEmpresa.findById(service.findById(id).get().getEmpresa().getCnpj()).get());
-    	return "vagas";
-    }
-    
-    @GetMapping("/removerVaga")
-    public String removerVaga(@RequestParam int id) {
-    	Vaga vaga = service.findById(id).get();
-    	service.delete(vaga);
-    	return "redirect:/listarVagas";
-    }
+		model.addAttribute("pageCounter", "Página " + numPaginaAtual + " de " + numTotalPaginas);
+		model.addAttribute("nextPage", ((pageable.getPageNumber() + 1) > numTotalPaginas - 1)
+				? pageable.getPageNumber()
+				: pageable.getPageNumber() + 1);
+		model.addAttribute("previousPage", pageable.getPageNumber() - 1);
+		model.addAttribute("quantidadePaginas", numTotalPaginas);
+
+		model.addAttribute("listaVagas", pgVagas);
+		return "listarVagas";
+	}
+
+	@GetMapping("/alterarVaga")
+	public String alterarVaga(@RequestParam int id, Model model) {
+		model.addAttribute("vaga", service.findById(id));
+		model.addAttribute("listaEmpresas",
+				repEmpresa.findById(service.findById(id).get().getEmpresa().getCnpj()).get());
+		return "vagas";
+	}
+
+	@GetMapping("/removerVaga")
+	public String removerVaga(@RequestParam int id) {
+		Vaga vaga = service.findById(id).get();
+		service.delete(vaga);
+		return "redirect:/listarVagas";
+	}
 }
--- src/main/java/application/vaga/Vaga.java
+++ src/main/java/application/vaga/Vaga.java
@@ -8,10 +8,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
 import application.empresa.Empresa;
@@ -32,72 +30,88 @@ public class Vaga {
 	@ManyToOne
 	@NotNull
 	private Empresa empresa;
-	
+
 	@OneToMany(mappedBy = "vaga", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<VagaPreenchida> vagasPreenchidas;
-	
-	
-	
+
 	public List<VagaPreenchida> getVagasPreenchidas() {
 		return vagasPreenchidas;
 	}
+
 	public void setVagasPreenchidas(List<VagaPreenchida> vagasPreenchidas) {
 		this.vagasPreenchidas = vagasPreenchidas;
 	}
+
 	public Empresa getEmpresa() {
 		return empresa;
 	}
+
 	public void setEmpresa(Empresa empresa) {
 		this.empresa = empresa;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getInterlocutor() {
 		return interlocutor;
 	}
+
 	public void setInterlocutor(String interlocutor) {
 		this.interlocutor = interlocutor;
 	}
+
 	public Integer getQuantidadeVagasMasculinas() {
 		return quantidadeVagasMasculinas;
 	}
+
 	public void setQuantidadeVagasMasculinas(int quantidadeVagasMasculinas) {
 		this.quantidadeVagasMasculinas = quantidadeVagasMasculinas;
 	}
+
 	public Integer getQuantidadeVagasFemininas() {
 		return quantidadeVagasFemininas;
 	}
+
 	public void setQuantidadeVagasFemininas(int quantidadeVagasFemininas) {
 		this.quantidadeVagasFemininas = quantidadeVagasFemininas;
 	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public Integer getCargaHoraria() {
 		return cargaHoraria;
 	}
+
 	public void setCargaHoraria(int cargaHoraria) {
 		this.cargaHoraria = cargaHoraria;
 	}
+
 	public String getProcuraPor() {
 		return procuraPor;
 	}
+
 	public void setProcuraPor(String procuraPor) {
 		this.procuraPor = procuraPor;
 	}
+
 	public String getRestricao() {
 		return restricao;
 	}
+
 	public void setRestricao(String restricao) {
 		this.restricao = restricao;
 	}
-	
-	
+
 }
