39fc2d685fb6b2958866d9e0c4f3478fe6b80983
fernando-castelo <nando.castelobranco24@hotmail.com>
2023-05-13 15:53:23 -0300

    adicao modal excluir e ajustes imgs index

--- src/main/resources/static/js/result.js
+++ src/main/resources/static/js/result.js
@@ -21,6 +21,14 @@ modal.addEventListener('click', function(event) {
 	}
 });
 
+$(document).on("click", "#botao-remover", function () {
+	var nome = $(this).data('nome');
+	$("#confirmNome").text(nome);
+	var cpf = $(this).data('cpf');
+	$("#botao-confirmar-remover").attr("href", "/removerApenado?cpf=" + cpf);
+	$('#confirmModal').modal('show');
+});
+
 
 function handleCheckbox(chbx){
 	const listaDeCheckboxesIndividuais = document.querySelectorAll("#individual-checkbox")
@@ -31,30 +39,29 @@ function handleCheckbox(chbx){
 	}
 }
 
-
 // FUNCAO QUE MOSTRA O MODAL
-function setModal(modalId, nomeDoApenado, cpfApenado){
-	const modal = document.getElementById(modalId);
-	document.getElementById("mensagem-de-confirmacao").innerText = `Você tem certeza que deseja excluir o registro de ${nomeDoApenado} definitivamente?`;
-	modal.classList.add("mostrar");
-	modal.addEventListener("click", (e) => {
-		if(e.target.id == modalId || e.target.className == "fechar" || e.target.className == "botao-nao"){
-			modal.classList.remove("mostrar")
-		} else if(e.target.className == "botao-sim"){
-			window.location.assign("www.google.com");
-		}
-	})
-}
-
-
-//tem que trocar o ".filtrar" por classe/id que tu colocar na imagem de lixeira
-const excluir = document.querySelectorAll("#botao-remover")
-excluir.forEach((i) => {
-	i.addEventListener("click", function(){
-		let nomeDoApenado = i.getAttribute("data-nome");
-		let cpfApenado = i.getAttribute("data-cpf");
-		setModal("modal", nomeDoApenado, cpfApenado);
-	})
-})
+// function setModal(modalId, nomeDoApenado, cpfApenado){
+// 	const modal = document.getElementById(modalId);
+// 	document.getElementById("mensagem-de-confirmacao").innerText = `Você tem certeza que deseja excluir o registro de ${nomeDoApenado} definitivamente?`;
+// 	modal.classList.add("mostrar");
+// 	modal.addEventListener("click", (e) => {
+// 		if(e.target.id == modalId || e.target.className == "fechar" || e.target.className == "botao-nao"){
+// 			modal.classList.remove("mostrar")
+// 		} else if(e.target.className == "botao-sim"){
+// 			window.location.assign("www.google.com");
+// 		}
+// 	})
+// }
+//
+//
+// //tem que trocar o ".filtrar" por classe/id que tu colocar na imagem de lixeira
+// const excluir = document.querySelectorAll("#botao-remover")
+// excluir.forEach((i) => {
+// 	i.addEventListener("click", function(){
+// 		let nomeDoApenado = i.getAttribute("data-nome");
+// 		let cpfApenado = i.getAttribute("data-cpf");
+// 		setModal("modal", nomeDoApenado, cpfApenado);
+// 	})
+// })
 
 
---
+++ src/main/resources/static/sql/apenado.sql
@@ -0,0 +1,82 @@
+ INSERT INTO `apenado` (`cpf`,`nome`,`nome_da_mae`,`data_nascimento`,`telefone`,`sexo_biologico`)
+VALUES
+  (68891531550,"Karleigh","","2023-11-18","14972413864","MASCULINO"),
+  (92532699480,"Hasad","","2022-10-28","22360741609","MASCULINO"),
+  (44248276007,"Josephine","","2024-02-02","18823887590","MASCULINO"),
+  (20540538336,"Cathleen","","2023-01-05","15524602021","MASCULINO"),
+  (84220451808,"Marsden","","2023-06-03","83144642468","MASCULINO"),
+  (18092483098,"Dean","","2023-07-20","35873137858","MASCULINO"),
+  (34544207594,"Winter","","2023-08-16","12428808043","MASCULINO"),
+  (76307126851,"Valentine","","2024-02-13","71282213635","MASCULINO"),
+  (57598551407,"Chadwick","","2022-07-01","03846011887","MASCULINO"),
+  (78250606147,"Callum","","2022-08-09","68127938415","MASCULINO");
+INSERT INTO `apenado` (`cpf`,`nome`,`nome_da_mae`,`data_nascimento`,`telefone`,`sexo_biologico`)
+VALUES
+    (51264803201,"Selma","","2023-07-25","35817302984","MASCULINO"),
+    (44649691485,"Hyatt","","2022-09-15","57135625736","MASCULINO"),
+    (87528689013,"Mariam","","2023-08-08","12961775727","MASCULINO"),
+    (97849097054,"Clinton","","2024-05-03","66632525223","MASCULINO"),
+    (71152933291,"Joel","","2022-05-16","79855241742","MASCULINO"),
+    (35737894831,"Igor","","2022-07-02","32158166726","MASCULINO"),
+    (88340152126,"Blake","","2024-03-14","19399423366","MASCULINO"),
+    (51188524992,"Hermione","","2023-10-03","45327233156","MASCULINO"),
+    (31331947769,"Aristotle","","2023-12-30","90482237747","MASCULINO"),
+    (52964502825,"Zachery","","2024-02-27","65286611355","MASCULINO");
+INSERT INTO `apenado` (`cpf`,`nome`,`nome_da_mae`,`data_nascimento`,`telefone`,`sexo_biologico`)
+VALUES
+    (71551308776,"Hall","","2024-02-10","77338384525","MASCULINO"),
+    (34700195038,"Evan","","2024-04-16","68950676348","MASCULINO"),
+    (43002715847,"Delilah","","2023-02-09","70724123432","MASCULINO"),
+    (85395314349,"Benedict","","2023-02-12","64234373565","MASCULINO"),
+    (88902016694,"Nash","","2022-12-14","57061151850","MASCULINO");
+[14:07, 08/05/2023] Fernando: INSERT INTO `apenado` (`cpf`,`nome`,`nome_da_mae`,`data_nascimento`,`telefone`,`sexo_biologico`)
+VALUES
+  (35811694131,"Germaine","Maria","2022-08-17","08943445789","MASCULINO"),
+  (85697073327,"Carson","Maria","2023-12-16","65560161281","MASCULINO"),
+  (14290194959,"Vladimir","Maria","2023-09-01","79784317031","MASCULINO"),
+  (73036034434,"Ryder","Maria","2023-07-20","49934022222","MASCULINO"),
+  (70556964602,"Randall","Maria","2023-03-19","35341895915","MASCULINO"),
+  (95192492160,"Elaine","Maria","2023-06-02","46177192639","MASCULINO"),
+  (72539999888,"Sage","Maria","2024-02-29","10565535753","MASCULINO"),
+  (53815378284,"Cain","Maria","2022-12-24","40713640647","MASCULINO"),
+  (38820462552,"Barbara","Maria","2023-04-09","54171428771","MASCULINO"),
+  (71722641920,"Chantale","Maria","2023-11-16","83245254635","MASCULINO");
+INSERT INTO `apenado` (`cpf`,`nome`,`nome_da_mae`,`data_nascimento`,`telefone`,`sexo_biologico`)
+VALUES
+    (89258451186,"Simon","Maria","2022-05-21","62831414714","MASCULINO"),
+    (79115802603,"Jaden","Maria","2023-06-09","12575889087","MASCULINO"),
+    (23545898990,"Tarik","Maria","2023-02-13","53481510229","MASCULINO"),
+    (36992448354,"Regan","Maria","2022-11-11","38479753531","MASCULINO"),
+    (74946283642,"Hedy","Maria","2022-11-03","96707675885","MASCULINO");
+
+INSERT INTO `apenado` (`cpf`,`nome`,`nome_da_mae`,`data_nascimento`,`telefone`,`sexo_biologico`)
+VALUES
+    (68891531550,"Karleigh","","2023-11-18","14972413864","MASCULINO"),
+    (92532699480,"Hasad","","2022-10-28","22360741609","MASCULINO"),
+    (44248276007,"Josephine","","2024-02-02","18823887590","MASCULINO"),
+    (20540538336,"Cathleen","","2023-01-05","15524602021","MASCULINO"),
+    (84220451808,"Marsden","","2023-06-03","83144642468","MASCULINO"),
+    (18092483098,"Dean","","2023-07-20","35873137858","MASCULINO"),
+    (34544207594,"Winter","","2023-08-16","12428808043","MASCULINO"),
+    (76307126851,"Valentine","","2024-02-13","71282213635","MASCULINO"),
+    (57598551407,"Chadwick","","2022-07-01","03846011887","MASCULINO"),
+    (78250606147,"Callum","","2022-08-09","68127938415","MASCULINO");
+INSERT INTO `apenado` (`cpf`,`nome`,`nome_da_mae`,`data_nascimento`,`telefone`,`sexo_biologico`)
+VALUES
+    (51264803201,"Selma","","2023-07-25","35817302984","MASCULINO"),
+    (44649691485,"Hyatt","","2022-09-15","57135625736","MASCULINO"),
+    (87528689013,"Mariam","","2023-08-08","12961775727","MASCULINO"),
+    (97849097054,"Clinton","","2024-05-03","66632525223","MASCULINO"),
+    (71152933291,"Joel","","2022-05-16","79855241742","MASCULINO"),
+    (35737894831,"Igor","","2022-07-02","32158166726","MASCULINO"),
+    (88340152126,"Blake","","2024-03-14","19399423366","MASCULINO"),
+    (51188524992,"Hermione","","2023-10-03","45327233156","MASCULINO"),
+    (31331947769,"Aristotle","","2023-12-30","90482237747","MASCULINO"),
+    (52964502825,"Zachery","","2024-02-27","65286611355","MASCULINO");
+INSERT INTO `apenado` (`cpf`,`nome`,`nome_da_mae`,`data_nascimento`,`telefone`,`sexo_biologico`)
+VALUES
+    (71551308776,"Hall","","2024-02-10","77338384525","MASCULINO"),
+    (34700195038,"Evan","","2024-04-16","68950676348","MASCULINO"),
+    (43002715847,"Delilah","","2023-02-09","70724123432","MASCULINO"),
+    (85395314349,"Benedict","","2023-02-12","64234373565","MASCULINO"),
+    (88902016694,"Nash","","2022-12-14","57061151850","MASCULINO");
