006b6426290fc3c9da80e893d3f3cab40192fc83
fernando-castelo <nando.castelobranco24@hotmail.com>
2023-05-19 15:47:15 -0300

    ajuste paginacao empresa

---
+++ src/main/resources/static/sql/empresa.sql
@@ -0,0 +1,75 @@
+INSERT INTO `empresa` (`cnpj`,`cidade`,`email`,`estado`,`interlocutor`,`nome`)
+VALUES
+    (24730,"Chastre","nisl.quisque.fringilla@protonmail.net","Turkey","Brittany Chambers","Diam Lorem Institute"),
+    (57736,"Uberlândia","eleifend.cras@google.couk","Poland","Abraham Williams","Pharetra Ut Company"),
+    (26013,"Khmelnytskyi","nibh.lacinia@icloud.net","Turkey","Hedley Schwartz","Sodales Purus Inc."),
+    (38249,"Hamilton","dictum.mi@yahoo.couk","Poland","Tobias Gaines","Mus Proin Inc."),
+    (20352,"Sengkang","elit.pellentesque@icloud.ca","Peru","Alea Gomez","Egestas Lacinia Sed Consulting"),
+    (13942,"Okene","eleifend.egestas.sed@google.net","Colombia","Leigh Schneider","Duis Elementum Associates"),
+    (90825,"Cà Mau","sit.amet.consectetuer@icloud.com","Costa Rica","Wyoming Andrews","Id Magna Et LLP"),
+    (89796,"Outram","risus.a@google.ca","China","Sandra Barnes","Senectus Et LLC"),
+    (11363,"Colorado Springs","suscipit@google.ca","Netherlands","Phelan Glass","Vitae PC"),
+    (82806,"Lelystad","feugiat.tellus@google.ca","Spain","Lee Vang","Nec Orci Foundation");
+INSERT INTO `empresa` (`cnpj`,`cidade`,`email`,`estado`,`interlocutor`,`nome`)
+VALUES
+    (85252,"Alice","convallis.in@hotmail.ca","Brazil","Linda Delacruz","Sed Consequat Corporation"),
+    (38460,"Santa Maria","faucibus@aol.edu","Italy","Blossom Velasquez","Eget Laoreet Ltd"),
+    (82692,"Calapan","suspendisse@protonmail.com","Belgium","Uriel Beard","Aliquam Tincidunt Institute"),
+    (85514,"Nässjö","elit.erat.vitae@protonmail.couk","Indonesia","Audrey Palmer","Mollis Dui LLP"),
+    (43976,"Denbigh","luctus@yahoo.com","South Africa","Libby Rose","Id Sapien PC"),
+    (59402,"Argenteuil","enim.mauris.quis@hotmail.org","Poland","Jade Castillo","Nam Ltd"),
+    (42682,"Lonquimay","mauris.suspendisse.aliquet@outlook.com","Peru","Bianca Bryan","Et Eros Company"),
+    (20573,"Mercedes","egestas.sed@outlook.edu","Singapore","Nigel Hayden","Quis Massa Limited"),
+    (30976,"Pelago","erat.eget@yahoo.ca","Belgium","Kyle Cruz","Fermentum Inc."),
+    (39307,"Iksan","magna.ut.tincidunt@icloud.net","Ireland","Finn Velazquez","Diam Pellentesque Limited");
+INSERT INTO `empresa` (`cnpj`,`cidade`,`email`,`estado`,`interlocutor`,`nome`)
+VALUES
+    (15626,"Hebei","nam@aol.com","Indonesia","Jocelyn Garner","Gravida Aliquam LLP"),
+    (11028,"Hunan","phasellus.dapibus.quam@google.net","Brazil","Travis Olsen","Fringilla Est Incorporated"),
+    (23913,"Andong","purus.ac@google.ca","Canada","Eliana Strong","Cursus Diam At LLP"),
+    (49391,"Cajamarca","dolor.quam@hotmail.couk","Germany","Haviva Gay","Ridiculus Mus Inc."),
+    (83286,"Imphal","donec.porttitor@outlook.ca","South Africa","Alfonso Hester","Est Ac Ltd"),
+    (38517,"Cádiz","facilisis@yahoo.net","United Kingdom","Brynn Mckenzie","Donec Corporation"),
+    (39175,"Warszawa","auctor.odio@icloud.com","Australia","Fitzgerald Powers","Cursus Non LLC"),
+    (4958,"Cartagena","sed.tortor.integer@icloud.net","Spain","Carly Buchanan","Ac Mattis Associates"),
+    (85039,"Tarnów","nulla.tempor@outlook.couk","Germany","Rosalyn Mathis","Consequat Nec Incorporated"),
+    (52888,"Gijón","curabitur.vel@protonmail.ca","Belgium","Tanisha Paul","Eget Ipsum Corp.");
+INSERT INTO `empresa` (`cnpj`,`cidade`,`email`,`estado`,`interlocutor`,`nome`)
+VALUES
+    (32098,"Xinjiang","id@yahoo.couk","Sweden","Yeo Powers","Aliquam Eu Accumsan PC"),
+    (69116,"Lago Verde","in.lorem.donec@yahoo.net","United Kingdom","Rafael Kelley","Mauris Morbi Institute"),
+    (32063,"Kurram Agency","quam.a@icloud.net","Indonesia","Stephen Herrera","Amet Faucibus Industries"),
+    (27568,"Mattersburg","semper.pretium@aol.net","Australia","Jael Parks","Ut Pharetra Corp."),
+    (4374,"Santander","lorem@icloud.ca","Brazil","Claire Lott","Est Mollis Non Industries");
+
+
+INSERT INTO `empresa` (`cnpj`,`cidade`,`email`,`estado`,`interlocutor`,`nome`)
+VALUES
+    (432,"Dublin","lorem.vitae.odio@icloud.ca","Peru","Batman","At Inc."),
+    (16661,"Santa Juana","sapien.nunc.pulvinar@icloud.couk","Sweden","Batman","Nisl Maecenas Associates"),
+    (465,"Tallahassee","per.inceptos@protonmail.com","India","Batman","Nunc Sit Limited"),
+    (63454,"Sarreguemines","sodales.elit@outlook.couk","Russian Federation","Batman","Urna Nunc Ltd"),
+    (99619,"Finkenstein am Faaker See","nec.mollis@aol.com","Norway","Batman","Nibh Vulputate Foundation"),
+    (83094,"Mohmand Agency","eget@icloud.ca","Turkey","Batman","Porttitor Eros Limited"),
+    (3151,"North Waziristan","ante.dictum@hotmail.net","South Korea","Batman","Suspendisse Industries"),
+    (41977,"Cedar Rapids","proin@outlook.ca","Brazil","Batman","Praesent Interdum LLC"),
+    (24125,"Bhusawal","inceptos@aol.edu","Austria","Batman","A PC"),
+    (5118,"Central Water Catchment","rhoncus@aol.org","Turkey","Batman","Elit LLP");
+
+INSERT INTO `empresa` (`cnpj`,`cidade`,`email`,`estado`,`interlocutor`,`nome`)
+VALUES
+    (46752,"Lipetsk","teste@email.com","South Africa","Batman","Hendrerit Neque Institute"),
+    (81531,"Stratford","teste@email.com","South Africa","Batman","Lacinia Industries"),
+    (52796,"Rawalakot","teste@email.com","Chile","Batman","A Company"),
+    (62856,"Coldstream","teste@email.com","Costa Rica","Batman","Ante Bibendum PC"),
+    (80480,"Doetinchem","teste@email.com","Philippines","Batman","Auctor Quis Incorporated"),
+    (9849,"Cork","teste@email.com","United States","Batman","Neque Nullam Incorporated"),
+    (42720,"Murcia","teste@email.com","Singapore","Batman","Lobortis Quis PC"),
+    (36704,"Watson Lake","teste@email.com","Vietnam","Batman","Donec Tempus LLP"),
+    (36186,"Purén","teste@email.com","China","Batman","Cum Corp."),
+    (52258,"Saint-Louis","teste@email.com","Turkey","Batman","Dictum Cursus Limited"),
+    (72308,"Wonju","teste@email.com","United Kingdom","Batman","Fusce Mi Lorem Incorporated"),
+    (54850,"Ensenada","teste@email.com","India","Batman","Molestie In Limited"),
+    (15780,"Bournemouth","teste@email.com","Indonesia","Batman","Varius Ultrices LLP"),
+    (24344,"Maracanaú","teste@email.com","Turkey","Batman","Et Corporation"),
+    (9755,"Piedras Negras","teste@email.com","France","Batman","Donec Fringilla Institute");
