5595bca8a6f23c78cb7a3bd1d066924ee1fd7b3e
Your Name <youremail@yourdomain.com>
2023-07-18 16:05:33 -0300

    Lógica de validação deixada apenas no backend. Padronização das mensagens de erro.

--- src/main/resources/static/js/alertReturnModal.js
+++ src/main/resources/static/js/alertReturnModal.js
@@ -15,70 +15,70 @@ document.querySelector('#cancelar').addEventListener('click', ()=>{
 })
 
 //validação cnpj
-document.addEventListener('DOMContentLoaded', function() {
-    var form = document.getElementById('formulario');
-    form.addEventListener('submit', function(event) {
-        var cnpjInput = document.getElementById('cnpj');
-        var nomeEmpresa = document.querySelector('#nomeEmpresa')
-        var cnpj = cnpjInput.value;
-        document.querySelector('#cnpjInvalido').setAttribute('style', 'display: none;');
-        document.querySelector('#nomeEmpresaInvalido').setAttribute('style', 'display: none;');
-        if (!validarCNPJ(cnpj)) {
-            event.preventDefault();
-            document.querySelector('#cnpjInvalido').setAttribute('style', 'display: block;');
-            return false;
-        }
-        if(nomeEmpresa.value == ''){
-            event.preventDefault();
-            document.querySelector('#nomeEmpresaInvalido').setAttribute('style', 'display: block;');
-            return false;
-        }
-    });
-});
-
-function validarCNPJ(cnpj) {
-    cnpj = cnpj.replace(/[^\d]+/g, '');
-
-    if (cnpj.length !== 14 || /^(.)\1+$/.test(cnpj)) {
-        return false;
-    }
-
-    var tamanho = cnpj.length - 2;
-    var numeros = cnpj.substring(0, tamanho);
-    var digitos = cnpj.substring(tamanho);
-    var soma = 0;
-    var pos = tamanho - 7;
-
-    for (var i = tamanho; i >= 1; i--) {
-        soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
-        if (pos < 2) {
-            pos = 9;
-        }
-    }
-
-    var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-
-    if (resultado !== parseInt(digitos.charAt(0))) {
-        return false;
-    }
-
-    tamanho = tamanho + 1;
-    numeros = cnpj.substring(0, tamanho);
-    soma = 0;
-    pos = tamanho - 7;
-
-    for (var i = tamanho; i >= 1; i--) {
-        soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
-        if (pos < 2) {
-            pos = 9;
-        }
-    }
-
-    resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-
-    if (resultado !== parseInt(digitos.charAt(1))) {
-        return false;
-    }
-
-    return true;
-}
+// document.addEventListener('DOMContentLoaded', function() {
+//     var form = document.getElementById('formulario');
+//     form.addEventListener('submit', function(event) {
+//         var cnpjInput = document.getElementById('cnpj');
+//         var nomeEmpresa = document.querySelector('#nomeEmpresa')
+//         var cnpj = cnpjInput.value;
+//         document.querySelector('#cnpjInvalido').setAttribute('style', 'display: none;');
+//         document.querySelector('#nomeEmpresaInvalido').setAttribute('style', 'display: none;');
+//         if (!validarCNPJ(cnpj)) {
+//             event.preventDefault();
+//             document.querySelector('#cnpjInvalido').setAttribute('style', 'display: block;');
+//             return false;
+//         }
+//         if(nomeEmpresa.value == ''){
+//             event.preventDefault();
+//             document.querySelector('#nomeEmpresaInvalido').setAttribute('style', 'display: block;');
+//             return false;
+//         }
+//     });
+// });
+//
+// function validarCNPJ(cnpj) {
+//     cnpj = cnpj.replace(/[^\d]+/g, '');
+//
+//     if (cnpj.length !== 14 || /^(.)\1+$/.test(cnpj)) {
+//         return false;
+//     }
+//
+//     var tamanho = cnpj.length - 2;
+//     var numeros = cnpj.substring(0, tamanho);
+//     var digitos = cnpj.substring(tamanho);
+//     var soma = 0;
+//     var pos = tamanho - 7;
+//
+//     for (var i = tamanho; i >= 1; i--) {
+//         soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
+//         if (pos < 2) {
+//             pos = 9;
+//         }
+//     }
+//
+//     var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+//
+//     if (resultado !== parseInt(digitos.charAt(0))) {
+//         return false;
+//     }
+//
+//     tamanho = tamanho + 1;
+//     numeros = cnpj.substring(0, tamanho);
+//     soma = 0;
+//     pos = tamanho - 7;
+//
+//     for (var i = tamanho; i >= 1; i--) {
+//         soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
+//         if (pos < 2) {
+//             pos = 9;
+//         }
+//     }
+//
+//     resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+//
+//     if (resultado !== parseInt(digitos.charAt(1))) {
+//         return false;
+//     }
+//
+//     return true;
+// }
